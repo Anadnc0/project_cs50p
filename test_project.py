3e81ed3575cd3ece6c3279182fcedcc6e38c1204
Ana Camacho <93103826+Anadnc0@users.noreply.github.com>
2024-10-29 22:49:39 -0300

    Delete test_project.py

--- test_project.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import main
-
-class TestMain(unittest.TestCase):
-    def test_do_stuff(self):
-        self.assertEqual(main.do_stuff(10), 15)
-
-if __name__ == '__main__':
-    unittest.main()
